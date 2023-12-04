85d1bdde9a3f4ce506249d68e0e58fa9c7ee9a9a
Joe Staller <joestaller23@gmail.com>
2023-12-04 11:09:24 -0500

    part two done

--- 2023/04-scratchcards/index.js
+++ 2023/04-scratchcards/index.js
@@ -2,9 +2,11 @@ const input = require("./input.js");
 
 const cards = input.split("\n");
 
-const parseCard = (card) => {
+const parseCard = (card, index) => {
   const [winNums, haveNums] = card.split(":")[1].trim().split("|");
   return {
+    cardNum: index + 1,
+    quantity: 1,
     winNums: winNums.split(" ").filter(Boolean).map(Number),
     haveNums: haveNums.split(" ").filter(Boolean).map(Number),
   };
@@ -19,8 +21,29 @@ const getPoints = ({ winNums, haveNums }) => {
   return score === 0.5 ? 0 : score;
 };
 
-const result = cards
-  .map(parseCard)
-  .reduce((prev, curr) => prev + getPoints(curr), 0);
+const getWins = ({ winNums, haveNums }) => {
+  let wins = 0;
+  haveNums.forEach((haveNum) => {
+    if (winNums.includes(haveNum)) wins++;
+  });
 
-console.log(result);
+  return wins;
+};
+
+const partOne = () =>
+  cards.map(parseCard).reduce((prev, curr) => prev + getPoints(curr), 0);
+
+const partTwo = () => {
+  const parsedCards = cards
+    .map(parseCard)
+    .map((parsedCard, index, allCards) => {
+      const wins = getWins(parsedCard);
+      for (let i = 0; i < wins; i++) {
+        allCards[index + i + 1].quantity += 1 * parsedCard.quantity;
+      }
+
+      return parsedCard;
+    });
+
+  return parsedCards.reduce((prev, curr) => prev + curr.quantity, 0);
+};
