d4bf142744506360d726ea821fc7886ec340e152
Joe Staller <joestaller23@gmail.com>
2023-12-06 22:11:20 -0500

    part one day 5 done

---
+++ 2023/05-fertilizer/index.js
@@ -0,0 +1,40 @@
+const { sortBy } = require("lodash");
+const input = require("./input");
+
+const [seedString, ...mappersStrings] = input.split("\n\n");
+
+const parseInitialSeeds = (string) =>
+  string.split(": ")[1].split(" ").map(Number);
+
+const parseMapping = (string) =>
+  string
+    .split(":")[1]
+    .split("\n")
+    .slice(1)
+    .map((row) => {
+      const [destStart, sourceStart, length] = row.split(" ").map(Number);
+      return { destStart, sourceStart, length };
+    });
+
+const seeds = parseInitialSeeds(seedString);
+const mappers = mappersStrings.map(parseMapping);
+
+const mapPipe = (source, mapper) => {
+  const foundMapping = mapper.find(
+    (mapping) =>
+      source >= mapping.sourceStart &&
+      source < mapping.sourceStart + mapping.length
+  );
+  if (foundMapping) {
+    // translate source to dest
+    return foundMapping.destStart - foundMapping.sourceStart + source;
+  }
+  return source;
+};
+
+const result = seeds.map((seed) =>
+  mappers.reduce((prev, curr) => mapPipe(prev, curr), seed)
+);
+
+console.log(sortBy(result));
+// console.log(mapPipe(seeds[3], mappers[0]));
