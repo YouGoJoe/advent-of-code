1a9a2a05ff009f56d56b211652693e1f927fe3d5
Joe Staller <joestaller23@gmail.com>
2023-12-09 11:50:48 -0500

    day 9 part one done

---
+++ 2023/09-mirage-maintenance/index.js
@@ -0,0 +1,42 @@
+const bigInput = require("./input");
+
+const input = `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`;
+
+const parseInput = (input) =>
+  input.split("\n").map((row) => row.split(" ").map(Number));
+
+const diffSteps = (steps) => {
+  const prevRow = steps[steps.length - 1];
+  if (prevRow.every((val) => val === 0)) {
+    // push the extra 0 to help with the summation step
+    prevRow.push(0);
+    return steps;
+  }
+  const nextRow = [];
+  for (let i = 0; i < prevRow.length - 1; i++) {
+    nextRow.push(prevRow[i + 1] - prevRow[i]);
+  }
+  steps.push(nextRow);
+  return diffSteps(steps);
+};
+
+const incrementSteps = (diffedSteps) => {
+  for (let i = 0; i < diffedSteps.length - 1; i++) {
+    const prevRow = diffedSteps[i];
+    const workingRow = diffedSteps[i + 1];
+    workingRow.push(
+      prevRow[prevRow.length - 1] + workingRow[workingRow.length - 1]
+    );
+  }
+  return diffedSteps;
+};
+
+const partOne = (input) =>
+  parseInput(input)
+    .map((row) => incrementSteps(diffSteps([row]).reverse()))
+    .reduce((prev, curr) => prev + curr.pop().pop(), 0);
+
+// console.log(partOne(input));
+console.log(partOne(bigInput));
